6c661c33769eddee0794ddc0e2092946e8fed5e7
Cristina Aguilera <developer.c.93.c@gmail.com>
2024-10-24 19:02:57 +0200

    ultimo arreglo 3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,11 +22,12 @@ function Navbar() {
   return (
     <nav className="navbar">
       <Link className="nav-link" to="/">Home</Link>
-      <Link className="nav-link" to="/contact">Contacto</Link>
+      
       {!isLoggedIn && <Link className="nav-link" to="/signup">Registro</Link>}
       {!isLoggedIn && <Link className="nav-link" to="/login">Acceso</Link>}
       {isLoggedIn && <Link className="nav-link" to="/user-profile">Mi perfil</Link>}
       {isLoggedIn && <span className="nav-link logout-link" onClick={handleLogout}>Cerrar sesión</span>}
+      <Link className="nav-link" to="/contact">Contacto</Link>
     </nav>
   );
 }
